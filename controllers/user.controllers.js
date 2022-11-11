7f2b058642dd41547d4c610c5ea8fc2afde99589
minhnhatle104 <lnminh104@gmail.com>
2022-11-12 02:32:18 +0700

    10_register_users

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,9 +1,14 @@
+const brcypt = require("bcryptjs")
 const {User} = require("../models")
 
 const register = async (req,res) => {
     const {name,email,password,numberPhone} = req.body
     try{
-        const newUser = await User.create({name,email,password,numberPhone,type:"client"})
+        // Tạo ra mỗi chuỗi ngẫu nhiên
+        const salt = brcypt.genSaltSync(10)
+        // mã hóa salt + password
+        const hashPassword = brcypt.hashSync(password,salt)
+        const newUser = await User.create({name,email,password:hashPassword,numberPhone})
         res.status(201).send(newUser)
     }catch(error){
         res.status(500).send(error)
